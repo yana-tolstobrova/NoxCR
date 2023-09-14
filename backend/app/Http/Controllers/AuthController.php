9eded29c48e4dc94a995360bbee90f4eea30fd06
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-09-14 12:01:02 +0200

    user authentication with sanctum, env configuration

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+    
+    public function register(RegisterRequest $request) {
+
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24); 
+
+        return response()->json([
+            'user' => new UserResource($user),
+        ])->withCookie($cookie);
+    }
+
+    
+    public function login(LoginRequest $request) {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Email or password is incorrect!'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24); 
+
+        return response()->json([
+            'user' => new UserResource($user),
+        ])->withCookie($cookie);
+    }
+
+    
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        $cookie = cookie()->forget('token');
+
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ])->withCookie($cookie);
+    }
+
+    
+    public function user(Request $request) {
+        return new UserResource($request->user());
+    }
+}
--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,15 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+    public function handle($request, Closure $next, ...$guards) {
+
+        if ($token = $request->cookie('token')) {
+            $request->headers->set('Authorization', 'Bearer ' . $token);
+        }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
     }
 }
