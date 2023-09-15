668579252b00bbfa65ded7adf6b060ae7be6adc3
Sylvia Llorente <sylviall81@gmail.com>
2023-09-15 20:48:25 +0200

    nueva prueba

--- backend/app/Http/Controllers/MailController.php
+++ backend/app/Http/Controllers/MailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Mail;
 use App\Mail\DemoMail;
+use App\Mail\orderConfirmation;
   
 class MailController extends Controller
 {
@@ -24,4 +25,22 @@ class MailController extends Controller
            
         dd("Email is sent successfully.");
     }
+
+    public function orderConfirmation()
+    {
+        $mailData = [
+            'pedido' => 'Test Mail',
+            'producto' => 'This is for testing email using smtp.',
+            'cantidad' => "1 par"
+        ];
+         
+        Mail::to('sylviall81@gmail.com')->send(new DemoMail($mailData));
+           
+        dd("Email is sent successfully.");
+    }
+
+
+
+
+
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\MailController;
  });
 
   
-Route::get('send-mail', [MailController::class, 'index']);
+Route::get('send-mail', [MailController::class, 'orderConfirmation']);
 
 
 
