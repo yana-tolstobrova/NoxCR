5f3eaffefb8bdcc57c5250e80cb5c53840564a01
mgblanco10 <mgblanco1001@gmail.com>
2023-09-18 08:39:39 +0200

    title products categories

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -10,9 +10,10 @@ function Main() {
 
   return (
     <div>
+      <h2 className="text-center mb-4 mt-4 text-2xl ml-4 font-bold" >Nuestros lentes de contacto</h2>
       <FilterProducts />
       <div className="flex justify-between items-center">
-        <h1 className="text-left mb-4 mt-4 text-2xl ml-4 font-bold" style={{ marginLeft: '240px' }}>Productos</h1>
+        <h2 className="text-left mb-4 mt-4 text-2xl ml-4 font-bold" style={{ marginLeft: '240px' }}>Productos</h2>
         <Link to="/products" className="flex items-center text-500 mr-4" style={{ marginRight: '295px' }}>
           Ver más
           <img className="p-2" src={arrow} alt="icono arrow" />
