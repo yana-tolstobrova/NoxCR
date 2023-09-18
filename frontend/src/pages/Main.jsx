6cb1f208d111f186ef4f7747b5818047092890dd
mgblanco10 <mgblanco1001@gmail.com>
2023-09-18 15:48:46 +0200

    finish component care

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -5,6 +5,7 @@ import arrow from '../assets/arrow.svg';
 import FilterProducts from '../components/FilterProducts';
 import Carousel from '../components/Carousel';
 import Accordion from '../components/Accordion';
+import Care from '../components/Care';
 
 
 function Main() {
@@ -19,12 +20,13 @@ function Main() {
         <h2 className="text-left mb-4 mt-4 text-2xl ml-4 font-bold" style={{ marginLeft: '240px' }}>Productos</h2>
         <Link to="/products" className="flex items-center text-500 mr-4" style={{ marginRight: '295px' }}>
           Ver más
-          <img className="p-2" src={arrow} alt="icono arrow" />
+          <img className="p-2" src={arrow} alt="icono arrow del carrusel" />
         </Link>
       </div>
       <div className="mb-4">
         <Card limit={visibleCards} />
       </div>
+      <Care />
       <Accordion />
     </div>
   );
