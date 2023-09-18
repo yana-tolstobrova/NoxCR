9f568b67052270167f4f183c963209b87bd70e09
Sylvia Llorente <sylviall81@gmail.com>
2023-09-18 20:17:41 +0200

    test_user_can_register passed

--- backend/tests/Feature/Api/AuthTest.php
+++ backend/tests/Feature/Api/AuthTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -11,10 +13,19 @@ class AuthTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+     use RefreshDatabase;
+
+    public function test_user_can_register(): void
+    {
+        $this->postJson('api/register', [
+            'name' => 'Elena',
+            'email'=>'ele@mail.com',
+            'password' => 'Elena98*',
+            'password_confirmation' => 'Elena98*'
+        ]);
+
+        $this->assertCount(1, User::all());
+       
     }
 }
