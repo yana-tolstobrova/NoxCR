2b257c938e18f08d872b2e3e633954b200a4d69e
Rosapedret2 <rosapedret2@gmail.com>
2023-09-18 13:07:23 +0200

    wip funcion search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react"
 import search from "../assets/search.svg"
 import { Link } from "react-router-dom"
 
-function Search() {
-    return (
-        <Link to="/">
-            <img src={search} alt="icon-search" /> 
-        </Link>
-    )
-}
 
-export default Search
+
+
+function Search() {
+    const [isInputVisible, setInputVisible] = useState(false);
+  
+    const toggleInput = () => {
+      setInputVisible(!isInputVisible);
+    };
+  
+    const handleMouseEnter = () => {
+      if (!isInputVisible) {
+        setInputVisible(true);
+      }
+    };
+  
+    const handleMouseLeave = () => {
+      if (!isInputVisible) {
+        setInputVisible(false);
+      }
+    };
+  
+    return (
+      <div
+        className="relative"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img
+          src={search}
+          alt="icon-search"
+          className="cursor-pointer"
+          onClick={toggleInput}
+        />
+        {isInputVisible && (
+          <input
+            type="text"
+            className="border rounded-lg p-2 absolute top-0 right-0 mt-10"
+            placeholder="Buscar..."
+          />
+        )}
+      </div>
+    );
+  }
+  
+  export default Search;
