8f023a2bae13a9c9500e4dc7ebfa4c7823244275
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-09-18 09:53:23 +0200

    routes change

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,7 +21,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-    Route::resource('products', ProductController::class);
 });
-});
+
+Route::resource('products', ProductController::class);
 
