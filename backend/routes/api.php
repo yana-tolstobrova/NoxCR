60bf96845e1a8cb9f08faa5686250352815622bc
Sylvia Llorente <sylviall81@gmail.com>
2023-09-19 12:11:54 +0200

    mailing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
 
 /*
 |--------------------------------------------------------------------------
