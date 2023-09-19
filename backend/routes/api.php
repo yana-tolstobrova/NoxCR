f1e60762b8f2fde5824cea1a01a6fe47115d6a41
Sylvia Llorente <sylviall81@gmail.com>
2023-09-19 12:01:08 +0200

    contraseña de aplicacion google

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('send-mail', [MailController::class, 'index']);
+Route::get('send-orderConfirmation', [MailController::class, 'orderConfirmation']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
