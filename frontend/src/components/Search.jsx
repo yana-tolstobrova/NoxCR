4641e5d59b5ff47b45646808f1590880b247b8fe
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-09-20 00:08:11 +0200

    search input styled

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -3,83 +3,64 @@ import searchService from '../services/searchService'; // Importa el servicio de
 import search from '../assets/search.svg';
 
 function Search() {
-  const [isInputVisible, setInputVisible] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+    const [isInputVisible, setInputVisible] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+  
+    const toggleInput = () => {
+        setInputVisible((prevValue) => !prevValue);
+    };
+  
+    const handleMouseEnter = () => {
+      if (!isInputVisible) {
+        setInputVisible(true);
+      }
+    };
+  
+    const handleMouseLeave = () => {
+      if (isInputVisible) {
+        setInputVisible(false);
+      }
+    };
 
-  const toggleInput = () => {
-    setInputVisible(!isInputVisible);
-  };
-
-  const handleMouseEnter = () => {
-    if (!isInputVisible) {
-      setInputVisible(true);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (!isInputVisible) {
-      setInputVisible(false);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  // const handleSearch = async () => {
-  //   try {
-  //     const results = await searchService.searchProducts(searchTerm);
-  //     setSearchResults(results);
-  //   } catch (error) {
-  //     console.error('Error en la solicitud de búsqueda:', error);
-  //   }
-  // };
-  const handleSearch = (e) => {
-    e.preventDefault();
-    window.location.href = `/search?query=${searchTerm}`;
-  };
-
-  return (
-    <div
-      className="relative"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <img
-        src={search}
-        alt="icon-search"
-        className="cursor-pointer h-5 px-3"
-        onClick={toggleInput}
-      />
-      {isInputVisible && (
-        <div>
-          <form onSubmit={handleSearch}>
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+      };
+    
+      // const handleSearch = async () => {
+      //   try {
+      //     const results = await searchService.searchProducts(searchTerm);
+      //     setSearchResults(results);
+      //   } catch (error) {
+      //     console.error('Error en la solicitud de búsqueda:', error);
+      //   }
+      // };
+      const handleSearch = (e) => {
+        e.preventDefault();
+        window.location.href = `/search?query=${searchTerm}`;
+      };
+  
+    return (
+      <div
+      className="flex items-center relative w-48"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {isInputVisible && (
           <input
-            type="text"
-            className="border rounded-lg p-2 absolute top-0 right-0 mt-10"
-            placeholder="Buscar..."
-            value={searchTerm}
-            onChange={handleInputChange}
+          type="text"
+          className="border rounded-lg py-2 pr-7 pl-2 w-48"
+          placeholder="Buscar..."
           />
-          <button onClick={handleSearch}>Buscar</button>
-          </form>
-          {/* //hay que pasarlo al otro componente */}
-          {searchResults.length > 0 && (
-            <div>
-              <h2>Resultados de la búsqueda:</h2>
-              <ul>
-                {searchResults.map((product) => (
-                  <li key={product.id}>{product.name}</li>
-                ))}
-              </ul>
-            </div>
-            // hata aqui
           )}
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default Search;
+          <img
+            src={search}
+            alt="icon-search"
+            className="cursor-pointer h-5 px-2 absolute right-0"
+            onClick={toggleInput}
+          />
+      </div>
+    );
+  }
+  
+  export default Search;
