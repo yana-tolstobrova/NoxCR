6c9f2b741524033d909ed1384c7857e9bf6dba2f
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-09-26 00:28:46 +0200

    TwLink component refactor

--- frontend/src/components/TwLink.jsx
+++ frontend/src/components/TwLink.jsx
@@ -1,5 +1,10 @@
-export function TwLink(props) {
+import { Link } from "react-router-dom";
+
+const Anchor = (props) => <a {...props} />
+
+export function TwLink({ as, children, ...rest }) {
+    const Comp = as === 'anchor' ? Anchor : Link;
     return (
-        <a className="text-black-500 hover:bg-gray-700 hover:text-white px-8 py-2 rounded-md text-sm font-medium cursor-pointer" href={props.href}>{props.children}</a>
+        <Comp className="text-black-500 hover:bg-gray-700 hover:text-white px-8 py-2 rounded-md text-sm font-medium cursor-pointer" {...rest}>{children}</Comp>
     )
 }
