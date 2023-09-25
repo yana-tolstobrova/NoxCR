7a3f26684992b296f4bd6377491abadd8321c56e
Sylvia Llorente <sylviall81@gmail.com>
2023-09-25 22:46:53 +0200

    error linea 17 tabla product_user_table

--- backend/database/migrations/2023_09_25_203336_create_product_user_table.php
+++ backend/database/migrations/2023_09_25_203336_create_product_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product_user', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('productgit add _id');
+            $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
         });
     }
