9a4216529a883b13118c358c0f9967dfcbac8cb6
Sylvia Llorente <sylviall81@gmail.com>
2023-09-27 12:43:55 +0200

    cometar icono de favoritos no terminado

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { cardsProducts } from '../services/ApiGetProducts';
 //import {ApiFavoritesService} from '../services/ApiFavoritesService';
 import { Link } from 'react-router-dom'; 
-import Like from '../assets/heart.svg'
+//import Like from '../assets/heart.svg'
 //import {useContext}
 
 function Card({ categoryFilter, limit, isUserLoggedIn }) {
@@ -37,7 +37,7 @@ function Card({ categoryFilter, limit, isUserLoggedIn }) {
             <div className="max-w-[222px] h-[350px] rounded overflow-hidden shadow-lg">
               <Link to={`/product/${product.id}`}>
                 <img className="w-[222px] h-[260px] object-cover" src={product.image} alt={product.name} />
-                <Link to={'/register'}><img src={Like} alt='icono favoritos' className='w-6 h-6 ml-2 mt-2'></img></Link>
+                {/*<Link to={'/register'}><img src={Like} alt='icono favoritos' className='w-6 h-6 ml-2 mt-2'></img></Link>*Favorite Icon work in progress*/}
                 
                 <div className="px-4 py-2 h-[80px]">
                   <div className="text-l mb-2">{product.name}</div>
