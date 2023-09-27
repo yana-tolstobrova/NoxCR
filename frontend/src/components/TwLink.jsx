ddd0faa60d5e187987df24d7c3dee634fc1caa56
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-09-27 10:53:37 +0200

    wip

--- frontend/src/components/TwLink.jsx
+++ frontend/src/components/TwLink.jsx
@@ -5,6 +5,6 @@ const Anchor = (props) => <a {...props} />
 export function TwLink({ as, children, ...rest }) {
     const Comp = as === 'anchor' ? Anchor : Link;
     return (
-        <Comp className="text-black-500 px-8 py-2 text-lg rounded-md text-sm font-semibold cursor-pointer ml-4 hover:font-bold hover:underline"style={{ color: '#55285A' }} {...rest}>{children}</Comp>
+        <Comp className="text-black-500 px-8 py-2 text-lg rounded-md text-sm font-semibold cursor-pointer hover:font-bold hover:underline"style={{ color: '#55285A' }} {...rest}>{children}</Comp>
     )
 }
