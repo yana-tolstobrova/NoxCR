7414b46dd09964a01c07fbd1126605366ad6094a
Sylvia Llorente <sylviall81@gmail.com>
2023-10-03 01:50:47 +0200

    mailing

--- backend/resources/views/mails/orderConfirmationMail.blade.php
+++ backend/resources/views/mails/orderConfirmationMail.blade.php
@@ -21,25 +21,23 @@ font-family:  'Noto Sans', sans-serif;
 
     <h2>Confirmación de Pedido</h2>
 
-    <h4>Datos del cliente:</h4>
-    <ul>
-    <li>Nombre: {{$orderData['name']}}</li>
-    <li>Documento de Identificación: {{$orderData['cedula']}}</li>
-    <li>Dirección de entrega: {{ $orderData['address'] }}</li>
-    <h4>Tipo de envío: {{ $orderData['shipping_type'] }}</h4>
-    </ul>
-    <h4>Recuerda que previo a la aceptacion de tu pedido, has confirmado:</h4>
+    <h5>Recuerda que previo a la aceptacion de tu pedido, has confirmado:</h5>
     <ul>
         <li>✅Ser Mayor de edad</li>
         <li>✅Conocer los cuidados, precauciones y medidas de limpieza de tus lentes de contacto</li>
         <li>✅Tener la solución multipropósito para desinfectarlos adecuadamente</li>          
     </ul>
-        
-   <h4> Desde NoxCR queremos reiterarte que tu salud visual es nuestra prioridad!
+    <h5> Desde NoxCR queremos reiterarte que tu salud visual es nuestra prioridad!<br>
    Por eso te recomendamos que -antes de recibir tu pedido- hagas <a href="/cuidados-nox.pdf" target="_blank" rel="noopener noreferrer"
           download= "cuidados-noxcr.pdf"> click AQUI </a>  para descargar y leer cuidadosamente
-    todas las normas de uso, higiene y mantenimiento de tus lentes de contacto noxCR.
-
+    todas las normas de uso, higiene y mantenimiento de tus lentes de contacto noxCR.</h5>
+    <h4>Datos del cliente:</h4>
+    <ul>
+    <li>Nombre: {{$orderData['name']}}</li>
+    <li>Documento de Identificación: {{$orderData['cedula']}}</li>
+    <li>Dirección de entrega: {{ $orderData['address'] }}</li>
+    <li><h4>Tipo de envío: {{ $orderData['shipping_type'] }}</h4></li>
+    </ul>
     <h4>Detalle de pedido: </h4>
     <?php
     foreach (json_decode($orderData['products']) as $item)
@@ -51,8 +49,7 @@ font-family:  'Noto Sans', sans-serif;
         <br>
 <?php
     }
-    ?>  <h3>Precio Total: ₡{{$orderData['total_amount']}}</h3>
-        
+    ?>  <h3>Precio Total: ₡{{$orderData['total_amount']}}</h3> 
     Si tienes cualquier pregunta,sugerencia o inquietud, no dudes en contactarnos por cualquiera de 
     nuestras redes sociales, nuestro <a href="http://wa.me/50684993726" target="_blank">whatsapp</a> o por correo a noxcrlentes@gmail.com </h4>
     <h3>Gracias por confiar en NoxCR!!</h3>
