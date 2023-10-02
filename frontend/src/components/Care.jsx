983bb444a1aabb3bfc06d18ccaf58ae83302d229
Mónica Blanco Calviño <mgblanco1001@gmail.com>
2023-10-03 01:55:15 +0200

    wip responsive size md

--- frontend/src/components/Care.jsx
+++ frontend/src/components/Care.jsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import careData from '../data/dataCareProducts';
-
-// function Care() {
-//   return (
-//     <div data-testid="care-component" className="mx-auto max-w-screen-xl px-4">
-//       <div className="flex justify-center space-x-6 m-10">
-//         {careData.map((care, index) => (
-//           <div key={index} className="bg-white p-4 pt-0 rounded-lg w-1/3"  data-testid={`care-item-${index}`}>
-//             <div className="flex justify-center mb-4">
-//               <img
-//                 src={require(`../assets/${care.img}`)}
-//                 alt="Imagen de cuidados"
-//                 className="w-36 h-36 object-cover rounded-full mb-2"
-//               />
-//             </div>
-//             <p className="text-left text-base mb-2" style= {{color:"#575757"}}>{care.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Care;
 import React from 'react';
 import careData from '../data/dataCareProducts';
 
--- frontend/src/pages/NaturalProductsPage.jsx
+++ frontend/src/pages/NaturalProductsPage.jsx
@@ -3,7 +3,7 @@ import Card from '../components/Card';
 
 function NaturalProductsPage() {
   return (
-    <div>
+    <div className="mx-8">
       <h1 className="text-center mb-8 mt-4 text-2xl ml-4 font-bold"style={{ color: "#3C2046" }}>Productos de la Colección Natural</h1>
       <Card collection="Natural" /> 
     </div>
