ad77ab78bb901d64497f2fb82fc2a779e1a0ebce
Sylvia Llorente <sylviall81@gmail.com>
2023-10-03 01:32:22 +0200

    mailing mvp

--- backend/resources/views/mails/orderConfirmationMail.blade.php
+++ backend/resources/views/mails/orderConfirmationMail.blade.php
@@ -11,71 +11,51 @@
 <style>
 
 body {
-
 font-family:  'Noto Sans', sans-serif;
-
 }
-
 </style>
 </head>
 
 <body>
     <img width="150rem" class="logo" src="{{'Nox.jpg'}}" alt="logo">
 
-    <h1>Confirmación de Pedido</h1>
+    <h2>Confirmación de Pedido</h2>
 
-    <h3>Bienvenido al resumen de su pedido No.: {{$orderData['order_id']}}</h3>
-
-    <h3>Datos del cliente:</h3>
+    <h4>Datos del cliente:</h4>
     <ul>
-    <li> Nombre: {{$orderData['name']}}</li>
-    <li> Documento de Identificación: {{$orderData['cedula']}}</li>
-    <li> Dirección de entrega: {{ $orderData['address'] }}</li>
-
-    <h3>Detalle del pedido: </h3>
-   
-    <li>Tipo de envio: {{ $orderData['shipping_type'] }}</li>
-
-    <?php
-    foreach (json_decode($orderData['products']) as $item)
-    {?>
-        <li>{{$item->product->name}}</li>
-<?php
-
-        
-        // $itemList .= $item->product->name . " ";
-        // $itemList .= $item->quantity . " ";
-        // $itemList .= $item->product->price . " ";
-        // $itemList .= "</br>";
-    }
-    ?>
-        
-    <!-- <li> Productos: {{ ($orderData['products']) }}</li>
-    <li> Productos: {{ $orderData['data'] }}</li> -->
-
-         
-    <li> Precio Total: {{ $orderData['total_amount']}}</li>
-        </ul>
-
-
-    <h4>Recuerda que previo a la aceptacion de tu pedido, has confirmado:    </h4>
+    <li>Nombre: {{$orderData['name']}}</li>
+    <li>Documento de Identificación: {{$orderData['cedula']}}</li>
+    <li>Dirección de entrega: {{ $orderData['address'] }}</li>
+    <h4>Tipo de envío: {{ $orderData['shipping_type'] }}</h4>
+    </ul>
+    <h4>Recuerda que previo a la aceptacion de tu pedido, has confirmado:</h4>
     <ul>
         <li>✅Ser Mayor de edad</li>
         <li>✅Conocer los cuidados, precauciones y medidas de limpieza de tus lentes de contacto</li>
-        <li>✅Tener la solución multipropósito para desinfectarlos adecuadamente</li>
-          
+        <li>✅Tener la solución multipropósito para desinfectarlos adecuadamente</li>          
     </ul>
         
-   <h4> Adicionalmente, aprovechamos este correo para recordarte que la salud 
-    y bienestar de tu vista es lo primordial para NoxCR!  Por eso te recomendamos que, antes de recibir tu pedido, hagas <a href="/cuidados-nox.pdf" target="_blank" rel="noopener noreferrer"
+   <h4> Desde NoxCR queremos reiterarte que tu salud visual es nuestra prioridad!
+   Por eso te recomendamos que -antes de recibir tu pedido- hagas <a href="/cuidados-nox.pdf" target="_blank" rel="noopener noreferrer"
           download= "cuidados-noxcr.pdf"> click AQUI </a>  para que descargar y leer cuidadosamente
     todas las normas de uso, higiene y mantenimiento de tus lentes de contacto noxCR.
-    
-    Si tienes cualquier pregunta,sugerencia o inquietud, no dudes en contactarnos por cualquiera de 
-    nuestras redes sociales, nuestro <a href="http://wa.me/50684993726" target="_blank">wasapp</a> o por correo a noxcrlentes@gmail.com </h4>
 
-     
-    <p>Gracias por confiar en NoxCR!!</p>
+    <h4>Detalle de pedido: </h4>
+    <?php
+    foreach (json_decode($orderData['products']) as $item)
+    {?>
+        <li>Categoría de Producto: {{$item->product->category}}</li>
+        <li>Nombre del producto: {{$item->product->name}}</li>
+        <li>Cantidad: {{$item->quantity}}</li>
+        <li>Precio unitario: ₡{{$item->product->price}}</li>
+        <br>
+<?php
+    }
+    ?>  <h3>Precio Total: ₡{{$orderData['total_amount']}}</h3>
+        
+    Si tienes cualquier pregunta,sugerencia o inquietud, no dudes en contactarnos por cualquiera de 
+    nuestras redes sociales, nuestro <a href="http://wa.me/50684993726" target="_blank">whatsapp</a> o por correo a noxcrlentes@gmail.com </h4>
+    <h3>Gracias por confiar en NoxCR!!</h3>
     
 </body>
 </html>
