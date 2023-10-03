d0c58c3557492a663171e0eb8fd30a50bf343ff6
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-10-04 01:01:23 +0200

    add to cart notification

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,7 +3,6 @@ import { cardsProducts, getPhotos } from '../services/ApiProducts';
 import { Link } from 'react-router-dom'; 
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
 import '../index.css';
 
 function Card({ categoryFilter, limit}) {
@@ -14,6 +13,7 @@ function Card({ categoryFilter, limit}) {
 	const [photos, setPhotos]= useState();
 	const { user } = useAuth();
   const navigate = useNavigate();
+  const [cartNotification, setCartNotification] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,6 +34,7 @@ function Card({ categoryFilter, limit}) {
 
     fetchData();
   }, [categoryFilter, limit]);
+
   const getProductPhoto = (productId) => {
     const productPhotos = photos.filter((photo) => photo.product_id === productId);
     if (productPhotos.length > 0) {
@@ -56,19 +57,35 @@ const handleAddToCart = (e, product) => {
       cart.push({ product, quantity: 1 });
     }
     localStorage.setItem("cart", JSON.stringify(cart));
-
+    showCartNotification(product.name);
     setCartCount(cart.reduce((total, item) => total + item.quantity, 0));
   }
 };
-    
+const showCartNotification = (productName) => {
+  setCartNotification(
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z" clip-rule="evenodd" />
+</svg>
+
+  );
+  setTimeout(() => {
+    setCartNotification(null);
+  }, 500);
+};
 
   return (
     <div className="mx-8" style={{ marginLeft: '150px', marginRight: '100px' }}>
+          
     <div className="flex flex-wrap -mx-2">
       {products.map((product) => (
         <div key={product.id} className="w-1/2 md:w-1/2 lg:w-1/4 px-2 mb-12">
           <div className="max-w-[222px] h-[350px] rounded overflow-hidden shadow-lg relative card-box">
               <div className="rounded bg-transparent w-full h-[260px] absolute left-[-100%] z-2 card-menu opacity-0 flex flex-col">
+                {cartNotification && (
+                  <div className="bg-green-400 rounded-full absolute right-2 top-2">
+                    {cartNotification}
+                  </div>
+                  )}
                 <svg alt='icono favoritos' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-12 p-2 fill-white hover:fill-black cursor-pointer'>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
