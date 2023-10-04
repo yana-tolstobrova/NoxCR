8515bc052f15456cf5cae5a0ff30618b7cc6e388
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-10-05 00:38:52 +0200

    admin create product form bugs fixed - 2 image inputs required, multiple form submition, + validation

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -86,7 +86,7 @@ const showCartNotification = () => {
                     {cartNotification}
                   </div>
                   )}
-                <svg alt='icono favoritos' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-28 p-2 fill-white hover:fill-black cursor-pointer md:w-24 2xl:w-[140px]'>
+                <svg alt='icono favoritos' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-28 p-2 fill-white hover:fill-black cursor-pointer md:w-24 lg:w-[140px]'>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
                 <Link to={`/product/${product.id}`} className='h-[75%]'></Link>
