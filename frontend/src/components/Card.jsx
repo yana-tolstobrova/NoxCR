11e4e5e90b7287ccee29f5ebc8063d9bee471bd4
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-10-04 23:49:28 +0200

    photo for card main page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -77,11 +77,15 @@ const showCartNotification = () => {
     <div className="mx-8 m-8 md:ml-12">
     <div className="flex flex-wrap -mx-2">
       {products.map((product) => (
+        
         <div key={product.id} className="w-1/2 md:w-1/2 lg:w-1/4 px-3 mb-12 flex justify-center">
           <div className="max-w-[362px] h-[520px] rounded overflow-hidden shadow-lg relative card-box md:max-w-[252px] md:h-[400px] 2xl:max-w-[530px]  2xl:h-[780px]">
-
-
               <div className="rounded bg-transparent w-full h-[340px] absolute z-2 card-menu opacity-0 flex flex-col md:h-[260px] 2xl:h-[495px]"> 
+              {cartNotification && (
+                  <div className="bg-green-400 rounded-full absolute right-2 top-2">
+                    {cartNotification}
+                  </div>
+                  )}
                 <svg alt='icono favoritos' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className='w-28 p-2 fill-white hover:fill-black cursor-pointer md:w-24 2xl:w-[140px]'>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
@@ -91,7 +95,7 @@ const showCartNotification = () => {
 
 
               <Link to={`/product/${product.id}`}>
-              <img className="w-[362px] h-[320px] text-lg object-cover md:h-[250px] 2xl:w-[530px] 2xl:h-[480px] " src={product.image} alt={product.name} />  
+              <img className="w-[362px] h-[320px] text-lg object-cover md:h-[250px] 2xl:w-[530px] 2xl:h-[480px] " src={getProductPhoto(product.id)} alt={product.name} />  
               <div className="px-4 py-2 h-[80px]">
                 <div className="text-4xl mb-1 mt-4 text-gray-800 md:text-2xl 2xl:text-5xl 2xl:mt-8">{product.name}</div>
                 <p className="text-4xl font-semibold md:text-2xl 2xl:text-5xl mt-4 2xl:mt-8" style={{ color: '#7C3973' }}>
