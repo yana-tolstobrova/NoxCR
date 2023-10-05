301044ec13d0f9875898a9b9443f838467e303ef
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-10-05 22:40:14 +0200

    update logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import logo from "../assets/Logo.svg"
 export function Header() {
 
  return (
-    <header className="flex h-36 items-center pr-10 md:bg-transparent bg-black"> 
+    <header className="flex h-36 items-center ml-10 pr-10 md:bg-transparent bg-black"> 
         <Link to="/">
             <img className="h-24 w-48 md:block hidden" src={logo} alt="Logo" /> 
         </Link>
