9e0df96793db0d4f2981352d13a9ac4a1082e759
Rosapedret2 <rosapedret2@gmail.com>
2023-10-05 22:35:14 +0200

    Components History

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
  return (
     <header className="flex h-36 items-center pr-10 md:bg-transparent bg-black"> 
         <Link to="/">
-            <img className="h-24 w-48 md:block hidden" src={logo} alt="Logo" /> 
+            <img className="h-24 w-48 pl-10 md:block hidden" src={logo} alt="Logo" /> 
         </Link>
         <NavBar />
     </header>
