c1efa461b758bbb5d2c0b5b16971195dd8c47da5
Mónica Blanco Calviño <mgblanco1001@gmail.com>
2023-10-05 12:07:25 +0200

    favorites heart

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -46,7 +46,7 @@ function Card({ categoryFilter, limit}) {
 		fetchPhotos();
 
     fetchData();
-  }, [categoryFilter, limit]);
+  }, [categoryFilter, limit, user]);
 
   const getProductPhoto = (productId) => {
     const productPhotos = photos.filter((photo) => photo.product_id === productId);
@@ -87,30 +87,50 @@ const showCartNotification = () => {
 };
 
 
-   const handleToggleFavorites = async (id, isFavorite) => {
+  //  const handleToggleFavorites = async (id, isFavorite) => {
 
-    console.log(id,isFavorite)
+  //   console.log(id,isFavorite)
 
-      try {
+  //     try {
 
-        await isFavorite ? removeFavorite(id) : addFavorite(id);
+  //       await isFavorite ? removeFavorite(id) : addFavorite(id);
       
-      } catch (error){
-        console.error("Error al manejar favoritos:", error);
+  //     } catch (error){
+  //       console.error("Error al manejar favoritos:", error);
 
-      }
-    
+  //     }
 
-   };
+  //  };
+  const handleToggleFavorites = async (id, isFavorite) => {
+    console.log(id, isFavorite);
+    try {
+      await isFavorite ? removeFavorite(id) : addFavorite(id);
+
+      // Actualiza el estado local isFavorite para el producto
+      const updatedProducts = products.map(product => {
+        if (product.id === id) {
+          return {
+            ...product,
+            isFavorite: !isFavorite // Invierte el estado actual
+          };
+        }
+        return product;
+      });
+
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.error("Error al manejar favoritos:", error);
+    }
+  };
   
 
   return (
-    <div className="mx-8 m-8 md:ml-12">
-    <div className="flex flex-wrap -mx-2">
+    <div className="mx-8 md:mx-12">
+    <div className="flex flex-wrap justify-center">
       {products.map((product) => (
-        <div key={product.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 w-1/4 px-2 mb-12">
-          <div className="max-w-[222px] h-[350px] rounded overflow-hidden shadow-lg relative card-box">
-              <div className="rounded bg-transparent w-full h-[260px] absolute left-[-100%] z-2 card-menu opacity-0 flex flex-col">
+        <div key={product.id} className="w-[260px] md:w-[190px] md:m-0 md:mb-6 lg:w-[262px] 2xl:w-[362px] m-4 px-2 mb-12 flex justify-center">
+          <div className="max-w-[362px] h-[420px] rounded overflow-hidden shadow-lg relative card-box md:max-w-[242px] md:h-[300px] 2xl:max-w-[330px] 2xl:h-[580px]">
+              <div className="rounded bg-transparent w-full h-[340px] absolute z-2 card-menu opacity-0 flex flex-col md:h-[260px] 2xl:h-[495px]">
               {cartNotification && (
                   <div className="bg-green-400 rounded-full absolute right-2 top-2">
                     {cartNotification}
@@ -124,7 +144,7 @@ const showCartNotification = () => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className={`w-12 p-2 ${product.isFavorite ? 'fill-black' : 'fill-white'} hover:fill-${product.isFavorite ? 'fill-white' : 'fill-black'} cursor-pointer transition-colors duration-300`} 
+                  className={`w-12 p-2 ${product.isFavorite ? 'fill-red' : 'fill-white'} cursor-pointer transition-colors duration-300`} 
                   >
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
@@ -144,14 +164,14 @@ const showCartNotification = () => {
                 </>
                 )}
                 <Link to={`/product/${product.id}`} className='h-[75%]'></Link>
-                <button onClick={(e) => handleAddToCart(e, product)} className="hover:bg-white hover:text-black border-black border py-6 bg-black text-white w-full text-xl md:py-4 2xl:py-10 2xl:text-3xl">Añadir al carrito</button>
+                <button onClick={(e) => handleAddToCart(e, product)} className="hover:bg-white hover:text-black border-black border mb-14 py-4 bg-black text-white w-full text-xl md:py-4 2xl:py-10 2xl:text-3xl">Añadir al carrito</button>
               </div>
   
               <Link to={`/product/${product.id}`}>
-              <img className="w-[362px] h-[320px] text-lg object-cover md:h-[250px] 2xl:w-[530px] 2xl:h-[480px] " src={getProductPhoto(product.id)} alt={product.name} />  
+              <img className="w-[262px] h-[260px] text-lg object-cover md:h-[190px] 2xl:w-[530px] 2xl:h-[350px]" src={getProductPhoto(product.id)} alt={product.name} />  
               <div className="px-4 py-2 h-[80px]">
-                <div className="text-4xl mb-1 mt-4 text-gray-800 md:text-2xl 2xl:text-5xl 2xl:mt-8">{product.name}</div>
-                <p className="text-4xl font-semibold md:text-2xl 2xl:text-5xl mt-4 2xl:mt-8" style={{ color: '#7C3973' }}>
+                <div className="text-2xl mb-1 mt-4 text-gray-800 md:text-lg 2xl:text-3xl 2xl:mt-8 md:mt-0">{product.name}</div>
+                <p className="text-2xl font-semibold md:text-lg 2xl:text-3xl md:mt-2 mt-4 2xl:mt-8" style={{ color: '#7C3973' }}>
                 ₡{product.price}
                 </p>
               </div>
@@ -167,6 +187,11 @@ const showCartNotification = () => {
 }
 
 export default Card;
+
+
+
+
+
 
 
 
